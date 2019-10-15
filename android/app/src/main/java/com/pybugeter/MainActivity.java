a24f7a9a32dfd4fadbcf1a77be2c01f9586215dd
Quinn1876 <qhodges0442@gmail.com>
2019-10-14 20:19:54 -0400

    renamed to pybudgeter

--- android/app/src/main/java/com/pybugeter/MainActivity.java
+++ android/app/src/main/java/com/pybugeter/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pybugeter;
+package com.pybudgeter;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "pybugeter";
+    return "pybudgeter";
   }
 }
--- android/app/src/main/java/com/pybugeter/MainApplication.java
+++ android/app/src/main/java/com/pybugeter/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pybugeter;
+package com.pybudgeter;
 
 import android.app.Application;
 import android.content.Context;
