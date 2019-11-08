09e26f2325552ccbda887041f249ec91bb76fed1
Quinn1876 <qhodges0442@gmail.com>
2019-11-07 21:02:36 -0500

    Working commit

---
+++ app/src/main/java/com/example/camera2tutorial/MainActivity.kt
@@ -0,0 +1,251 @@
+package com.example.camera2tutorial
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraDevice.TEMPLATE_PREVIEW
+import android.hardware.camera2.CameraManager
+import android.media.Image
+import android.media.ImageReader
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Base64
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.widget.Toast
+//import com.android.volley.Request
+//import com.android.volley.Response
+//import com.android.volley.toolbox.JsonObjectRequest
+//import com.android.volley.toolbox.JsonRequest
+//import com.android.volley.toolbox.StringRequest
+//import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        surfaceView.holder.addCallback(surfaceReadyCallback)
+
+
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+            return
+        }
+
+//        if (!InternetPermissionHelper.hasInternetPermission(this)) {
+//            InternetPermissionHelper.requestInternetPermission(this)
+//            return
+//        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+
+        recreate()
+    }
+
+    private fun startCameraSession() {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        if (cameraManager.cameraIdList.isEmpty()) {
+            // no cameras
+            return
+        }
+        val firstCamera = cameraManager.cameraIdList[0]
+        try {
+            cameraManager.openCamera(firstCamera, object : CameraDevice.StateCallback() {
+                override fun onDisconnected(p0: CameraDevice) {}
+                override fun onError(p0: CameraDevice, p1: Int) {}
+
+                override fun onOpened(cameraDevice: CameraDevice) {
+                    // use the camera
+                    val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraDevice.id)
+
+                    cameraCharacteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]?.let { streamConfigurationMap ->
+                        streamConfigurationMap.getOutputSizes(ImageFormat.JPEG)?.let { jpgSizes ->
+                            val previewSize = jpgSizes[0]
+                            val displayRotation = windowManager.defaultDisplay.rotation
+                            val swappedDimensions = areDimensionsSwapped(displayRotation, cameraCharacteristics)// swap width and height if needed
+                            val rotatedPreviewWidth = if (swappedDimensions) previewSize.height else previewSize.width
+                            val rotatedPreviewHeight = if (swappedDimensions) previewSize.width else previewSize.height
+                            surfaceView.holder.setFixedSize(rotatedPreviewWidth, rotatedPreviewHeight)
+
+                            // Configure Image Reader
+                            val imageReader = ImageReader.newInstance(rotatedPreviewWidth, rotatedPreviewHeight,
+                                ImageFormat.JPEG, 2)
+
+                            val previewSurface = surfaceView.holder.surface
+                            val recordingSurface = imageReader.surface
+
+
+                            imageReader.setOnImageAvailableListener({
+                                // do something
+                                imageReader.acquireLatestImage()?.let { image ->
+                                    // TODO impliment
+                                    //val imagePlane = image.planes[0]
+                                    image.close()
+                                }
+                            }, Handler { true })
+
+                            val captureCallback = object : CameraCaptureSession.StateCallback()
+                            {
+                                override fun onConfigureFailed(session: CameraCaptureSession) {}
+
+                                override fun onConfigured(session: CameraCaptureSession) {
+                                    // session configured
+                                    val previewRequestBuilder =   cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+                                            addTarget(previewSurface)
+                                            addTarget(recordingSurface)
+                                        }
+                                    session.setRepeatingRequest(
+                                        previewRequestBuilder.build(),
+                                        object: CameraCaptureSession.CaptureCallback() {},
+                                    Handler { true }
+                                    )
+
+                                }
+                            }
+
+                            cameraDevice.createCaptureSession(mutableListOf(previewSurface, recordingSurface), captureCallback, Handler { true })
+                        }
+
+                    }
+                }
+            }, Handler { true })
+        }
+        catch( e: SecurityException) {
+
+        }
+    }
+
+
+
+    private fun areDimensionsSwapped(displayRotation: Int, cameraCharacteristics: CameraCharacteristics): Boolean {
+        var swappedDimensions = false
+        when (displayRotation) {
+            Surface.ROTATION_0, Surface.ROTATION_180 -> {
+                if (cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 90 || cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 270) {
+                    swappedDimensions = true
+                }
+            }
+            Surface.ROTATION_90, Surface.ROTATION_270 -> {
+                if (cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 0 || cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) == 180) {
+                    swappedDimensions = true
+                }
+            }
+            else -> {
+                // invalid display rotation
+            }
+        }
+        return swappedDimensions
+    }
+
+    val surfaceReadyCallback = object: SurfaceHolder.Callback {
+        override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) { }
+        override fun surfaceDestroyed(p0: SurfaceHolder?) { }
+
+        override fun surfaceCreated(p0: SurfaceHolder?) {
+            startCameraSession()
+        }
+    }
+   //  val queue = Volley.newRequestQueue(this)
+
+    /*
+    fun sendImage(image: Image.Plane) {
+        val url = "" // The endpoint for the flask server
+        val base64EncodedImage = Base64.encodeToString(image.buffer.array(), 0)
+        val bodyMap = HashMap<String, String>()
+        bodyMap["image"] = base64EncodedImage
+
+        val body = JSONObject(bodyMap)
+
+        val postRequest = JsonObjectRequest(Request.Method.POST, url, body,
+            Response.Listener<JSONObject>() {
+                response: JSONObject -> "" // Do something with the respoce
+            }, Response.ErrorListener {  }
+            )
+    }
+    */
+}
+
+/** Helper to ask camera permission.  */
+object CameraPermissionHelper {
+    private const val CAMERA_PERMISSION_CODE = 0
+    private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+
+    /** Check to see we have the necessary permissions for this app.  */
+    fun hasCameraPermission(activity: Activity): Boolean {
+        return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
+    fun requestCameraPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(
+            activity, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_CODE)
+    }
+
+    /** Check to see if we need to show the rationale for this permission.  */
+    fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)
+    }
+
+    /** Launch Application Setting to grant permission.  */
+    fun launchPermissionSettings(activity: Activity) {
+        val intent = Intent()
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        intent.data = Uri.fromParts("package", activity.packageName, null)
+        activity.startActivity(intent)
+    }
+}
+
+/** Helper to ask camera permission.  */
+object InternetPermissionHelper {
+    private const val INTERNET_PERMISSION_CODE = 0
+    private const val INTERNET_PERMISSION = Manifest.permission.INTERNET
+
+    /** Check to see we have the necessary permissions for this app.  */
+    fun hasInternetPermission(activity: Activity): Boolean {
+        return ContextCompat.checkSelfPermission(activity, INTERNET_PERMISSION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
+    fun requestInternetPermission(activity: Activity) {
+        ActivityCompat.requestPermissions(
+            activity, arrayOf(INTERNET_PERMISSION), INTERNET_PERMISSION_CODE)
+    }
+
+    /** Check to see if we need to show the rationale for this permission.  */
+    fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, INTERNET_PERMISSION)
+    }
+
+    /** Launch Application Setting to grant permission.  */
+    fun launchPermissionSettings(activity: Activity) {
+        val intent = Intent()
+        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        intent.data = Uri.fromParts("package", activity.packageName, null)
+        activity.startActivity(intent)
+    }
+}
